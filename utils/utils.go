ad693e550e325fc2c6a17c1272e406797b9cc0bb
Omar <omar.suleimani@rihal.om>
2023-08-16 17:45:54 +0400

    feat(server): Provide the top 5 occurring words

--- utils/utils.go
+++ utils/utils.go
@@ -85,7 +85,7 @@ func CountFilteredWords(text string) map[string]int {
 		"very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't",
 		"what", "what's", "when", "when's", "where", "where's", "which", "while", "who",
 		"who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd",
-		"you'll", "you're", "you've", "your", "yours", "yourself", "yourselves",
+		"you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", "can",
 	}
 
 	filteredWords := map[string]int{}
