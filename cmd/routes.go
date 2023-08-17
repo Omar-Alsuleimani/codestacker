7cd7f57cf666f8b7258bd6e093f45b40b2ffc5c6
Omar <omar.suleimani@rihal.om>
2023-08-17 23:21:27 +0400

    feat(server): Delete a pdf from minio and postgre

--- cmd/routes.go
+++ cmd/routes.go
@@ -27,7 +27,7 @@ func start() {
 
 	app.Get("/getPDF/:id/:page", handlers.GetPdfPage)
 
-	app.Post("/deletePDF", handlers.DeleteFile)
+	app.Delete("/deletePDF", handlers.DeleteFile)
 
 	app.Listen(":3000")
 }
